8d0fe8fa96761ef8debcef362b21e9fa33ce6237
Gerhard Z <zhenggenghao@idaoben.com>
2018-03-31 14:42:01 +0800

    初始化工程

---
+++ EnvironmentalMonitoring/EnvironmentalMonitoring/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EnvironmentalMonitoring
+//
+//  Created by Gerhard Z on 2018/3/31.
+//  Copyright © 2018年 lakers JH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EnvironmentalMonitoring/EnvironmentalMonitoring/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EnvironmentalMonitoring
+//
+//  Created by Gerhard Z on 2018/3/31.
+//  Copyright © 2018年 lakers JH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
