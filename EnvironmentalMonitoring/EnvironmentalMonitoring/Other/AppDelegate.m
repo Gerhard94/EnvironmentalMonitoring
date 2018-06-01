2b174a39fb16a395000ee9ecb426ff9345c6566d
Gerhard Z <zhenggenghao@idaoben.com>
2018-06-01 20:31:28 +0800

    修复OneNet调用出现bug

--- EnvironmentalMonitoring/EnvironmentalMonitoring/Other/AppDelegate.m
+++ EnvironmentalMonitoring/EnvironmentalMonitoring/Other/AppDelegate.m
@@ -65,7 +65,7 @@
     bar.translucent = NO;
     [bar setTitleTextAttributes:@{NSFontAttributeName : [UIFont fontWithName:@"Helvetica-Bold" size:18.0],NSForegroundColorAttributeName : [UIColor whiteColor]}];
     
-    [self cleanAlertNumber];
+//    [self cleanAlertNumber];
     
     //或者用这个都行
     
@@ -92,7 +92,7 @@
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    [self cleanAlertNumber];
+//    [self cleanAlertNumber];
 
 }
 
